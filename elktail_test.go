3135b0ff54137bff50e064662cd2246dae3ef240
Conner Peirce <me@connerpeirce.com>
2017-05-07 18:25:01 -0400

    Cover FormatRegexp for complete-ness

--- elktail_test.go
+++ elktail_test.go
@@ -24,9 +24,29 @@ func TestResolveField(t *testing.T) {
 	testutils.AssertEqualsString(t, "", eval(model1, "bar"))
 }
 
-
-
 func eval(model interface{}, expr string) string {
 	result, _ := EvaluateExpression(model, expr)
 	return result
+}
+
+func TestFormatRegexp(t *testing.T) {
+	formatString := "%timestamp %message[25] %trace[10] %error"
+
+	match := FormatRegexp.FindAllStringSubmatch(formatString, -1)
+
+	testutils.AssertEqualsString(t, "%timestamp", match[0][0])
+	testutils.AssertEqualsString(t, "%timestamp", match[0][1])
+	testutils.AssertEqualsString(t, "", match[0][2])
+
+	testutils.AssertEqualsString(t, "%message[25]", match[1][0])
+	testutils.AssertEqualsString(t, "%message", match[1][1])
+	testutils.AssertEqualsString(t, "25", match[1][2])
+
+	testutils.AssertEqualsString(t, "%trace[10]", match[2][0])
+	testutils.AssertEqualsString(t, "%trace", match[2][1])
+	testutils.AssertEqualsString(t, "10", match[2][2])
+
+	testutils.AssertEqualsString(t, "%error", match[3][0])
+	testutils.AssertEqualsString(t, "%error", match[3][1])
+	testutils.AssertEqualsString(t, "", match[3][2])
 }
